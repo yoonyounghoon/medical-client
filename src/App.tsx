1117bda5fb9774d8ffa55a0baaf64c88314a4745
yoonyounghoon <45310468+yoonyounghoon@users.noreply.github.com>
2021-08-06 00:24:47 +0900

    Implements: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Footer from 'components/Footer/Footer';
+import PageNotFound from 'components/PageNotFound';
+import Sidebar from 'components/Sidebar';
+import CovidPage from 'pages/CovidPage';
+import MedicalCheckPage from 'pages/MedicalCheckPage';
+import PharmacyPage from 'pages/PharmacyPage';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact component={MedicalCheckPage} />
+        <Route path="/covid" component={CovidPage} />
+        <Route path="/pharmacy" component={PharmacyPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
