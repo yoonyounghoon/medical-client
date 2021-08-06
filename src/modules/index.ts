51314448c55290b17656fed136a4e492fdc28054
yoonyounghoon <45310468+yoonyounghoon@users.noreply.github.com>
2021-08-06 13:15:24 +0900

    리덕스 사가 연결

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import position from './position';
+import createSagaMiddleware from 'redux-saga';
 
 const rootReducer = combineReducers({
   position: position.reducer,
 });
-
 export default rootReducer;
+
+export const sagaMiddleware = createSagaMiddleware();
 
 export type RootState = ReturnType<typeof rootReducer>;
