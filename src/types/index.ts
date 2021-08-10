74c31d4cd59536f51d5677bc089d54da421a845e
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-10 13:13:39 +0900

    types

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export type PharmacyInfo = {
+  cnt: number;
+  distance: number;
+  dutyAddr: string;
+  dutyDiv: string;
+  dutyDivName: string;
+  dutyName: string;
+  dutyTel1: string;
+  endTime: number;
+  hpid: string;
+  latitude: number;
+  longitude: number;
+  rnum: number;
+  startTime: string;
+};
+
+export type HospitalInfo = {
+  bcExmdChrgTypeCd: number;
+  ccExmdChrgTypeCd: number;
+  cvxcaExmdChrgTypeCd: number;
+  cxVl: number;
+  cyVl: number;
+  exmdrFaxNo: string;
+  exmdrTelNo: string;
+  grenChrgTypeCd: number;
+  hmcNm: string;
+  hmcNo: number;
+  hmcTelNo: string;
+  ichkChrgTypeCd: number;
+  locAddr: string;
+  locPostNo: string;
+  lvcaExmdChrgTypeCd: number;
+  mchkChrgTypeCd: number;
+  siDoCd: number;
+  siGunGuCd: number;
+  stmcaExmdChrgTypeCd: number;
+  ykindnm: string;
+};
