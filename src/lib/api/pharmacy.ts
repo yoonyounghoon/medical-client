d132f39b28f39ed8e1efd7355668611dbb50fc9a
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-10 18:50:16 +0900

    Implements: 약국 API 추가

--- src/lib/api/pharmacy.ts
+++ src/lib/api/pharmacy.ts
@@ -5,10 +5,16 @@ export type PositionData = {
   yPos: number;
 };
 
-//  약국 api
+//  가까운 약국 10개 찾아오는 API
 export const getPharmacyList = async (position: PositionData) => {
   const response = await axios.get(
     `/pharmacy/${position.yPos}/${position.xPos}`,
   );
+  return response.data;
+};
+
+// 약국 자세한 정보 API
+export const getPharmacyDetail = async (id: string) => {
+  const response = await axios.get(`/pharmacy/${id}`);
   return response.data;
 };
