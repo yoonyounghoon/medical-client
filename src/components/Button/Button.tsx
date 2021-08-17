52d61457d4f5651f2b868080d2ee82691372e68a
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-17 17:49:33 +0900

    Implements: 공통 버튼

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #3095e9;
+  border: none;
+  color: white;
+  height: 100%;
+`;
+
+export default Button;
