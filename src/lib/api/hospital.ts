97a93114493c98720fbec0e73a6fa807f9680ef6
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-18 18:45:17 +0900

    요청 주소 변경

--- src/lib/api/hospital.ts
+++ src/lib/api/hospital.ts
@@ -8,13 +8,13 @@ export type locationCode = {
 // 지역별 검진기관 API
 export const getHospitalListAPI = async (location: locationCode) => {
   const response = await axios.get(
-    `/hospital/${location.siDoCd}/${location.siGunGuCd}`,
+    `http://ec2-13-125-211-222.ap-northeast-2.compute.amazonaws.com:5000/hospital/${location.siDoCd}/${location.siGunGuCd}`,
   );
   return response.data;
 };
 
 // 병원별 자세한 정보
 export const getHospitalDetail = async (name: string) => {
-  const response = await axios.get(`/hospital/${name}`);
+  const response = await axios.get(`http://ec2-13-125-211-222.ap-northeast-2.compute.amazonaws.com:5000/hospital/${name}`);
   return response.data;
 };
--- src/lib/api/pharmacy.ts
+++ src/lib/api/pharmacy.ts
@@ -8,13 +8,13 @@ export type PositionData = {
 //  가까운 약국 10개 찾아오는 API
 export const getPharmacyList = async (position: PositionData) => {
   const response = await axios.get(
-    `/pharmacy/${position.yPos}/${position.xPos}`,
+    `http://ec2-13-125-211-222.ap-northeast-2.compute.amazonaws.com:5000/pharmacy/${position.yPos}/${position.xPos}`,
   );
   return response.data;
 };
 
 // 약국 자세한 정보 API
 export const getPharmacyDetail = async (id: string) => {
-  const response = await axios.get(`/pharmacy/${id}`);
+  const response = await axios.get(`http://ec2-13-125-211-222.ap-northeast-2.compute.amazonaws.com:5000/pharmacy/${id}`);
   return response.data;
 };
